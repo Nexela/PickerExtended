45b6a3621677f14ac0d33c76613a9aa81011d48c
Nicholas Dunn <nexela@gmail.com>
2017-12-16 19:34:13 -0500

    Fix area error in stdlib

--- picker/tools.lua
+++ picker/tools.lua
@@ -2,19 +2,18 @@
 --[[Tape Measure]]--
 -------------------------------------------------------------------------------
 local Area = require("stdlib.area.area")
-local Position = require("stdlib.area.position")
 
 local function measure_area(event)
     if event.item == "picker-tape-measure" then
         local player = game.players[event.player_index]
-        local area
+        local area = Area(event.area)
         if event.name == defines.events.on_player_selected_area then
-            area = Area.round_to_integer(event.area)
+            area:round_to_integer()
         else
-            area = Area.tile_center_points(event.area)
+            area:tile_center_points()
         end
-        local size, width, height = Area.size(area)
-        player.print(Area.tostring(area) .. " Center = "..Position.tostring(Area.center(area)))
+        local size, width, height = area:size()
+        player.print(area .. " Center = " .. area:center())
         player.print("Size = "..size.." Width = "..width.." Height = "..height)
     end
 end
