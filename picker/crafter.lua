5a96c8b111dab741ded6b976bf16bf9d293719ad
Nicholas Dunn <nexela@gmail.com>
2018-04-14 16:22:13 -0400

    Don't craft items if something is in your hand. Fixes #75

--- picker/crafter.lua
+++ picker/crafter.lua
@@ -6,7 +6,7 @@ local lib = require('picker/lib')
 
 local function picker_crafter(event)
     local player = game.players[event.player_index]
-    if player.selected then
+    if player.selected and not player.cursor_stack.valid_for_read then
         local _, _, ip = lib.get_placeable_item(player.selected)
         if ip and game.recipe_prototypes[ip.name] and player.force.recipes[ip.name].enabled and player.get_item_count(ip.name) == 0 then
             player.begin_crafting {count = 1, recipe = ip.name, silent = false}
