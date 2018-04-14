cfc4f4fa7c834a27dadc591a255bdcf4f79494fd
Nicholas Dunn <nexela@gmail.com>
2018-04-14 15:21:39 -0400

    Better Logic

--- picker/switchgun.lua
+++ picker/switchgun.lua
@@ -5,14 +5,15 @@ local Event = require('stdlib/event/event')
 
 local function switch_player_gun_while_driving(event)
     local player = game.players[event.player_index]
-    if player.character and player.driving then
-        local index = player.character.selected_gun_index
-        local gun_inv = player.character.get_inventory(defines.inventory.player_guns)
+    local character = player.character
+    if character and player.vehicle then
+        local index = character.selected_gun_index
+        local gun_inv = character.get_inventory(defines.inventory.player_guns)
         local start = index
         repeat
             index = index < #gun_inv and (index + 1) or 1
             if gun_inv[index].valid_for_read then
-                player.character.selected_gun_index = index
+                character.selected_gun_index = index
                 break
             end
         until index == start
