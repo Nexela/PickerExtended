f01dcba6a420a201d46ffe7456ad361f81bf8bcc
Nicholas Dunn <nexela@gmail.com>
2018-04-15 15:20:42 -0400

    Fix Sort Inventory, Fixes #81 Allow manually sorting player inventory

--- picker/sortinventory.lua
+++ picker/sortinventory.lua
@@ -1,10 +1,19 @@
 local Event = require('stdlib/event/event')
 
+local types = {
+    ['car'] = defines.inventory.car_trunk,
+    ['cargo-wagon'] = defines.inventory.cargo_wagon,
+    ['container'] = defines.inventory.chest,
+    ['logistic-container'] = defines.inventory.chest
+}
+
 local function sort_inventory(event)
     local player = game.players[event.player_index]
-    if player.opened and player.opened_gui_type == defines.gui_type.entity then
-        local inventory = player.opened.get_inventory(defines.inventory.car_trunk) or player.opened.get_inventory(defines.inventory.cargo_wagon) or player.opened.get_inventory(defines.inventory.chest)
-        if inventory and (event.input_name == 'picker-manual-inventory-sort' or player.mod_settings['picker-auto-sort-inventory'].value) then
+    if player.opened_self and event.input_name == 'picker-manual-inventory-sort' then
+        player.get_main_inventory().sort_and_merge()
+    elseif player.opened and player.opened_gui_type == defines.gui_type.entity then
+        local inventory = player.opened.get_inventory(types[player.opened.type] or 255)
+        if inventory and not inventory.is_empty() and (event.input_name == 'picker-manual-inventory-sort' or player.mod_settings['picker-auto-sort-inventory'].value) then
             inventory.sort_and_merge()
         end
     end
