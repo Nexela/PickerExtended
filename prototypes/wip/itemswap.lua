60df51acf3d41141eb09823634fb09f7f51c8715
Nicholas Dunn <nexela@gmail.com>
2018-04-28 21:08:45 -0400

    WIP layout stuff

---
+++ prototypes/wip/itemswap.lua
@@ -0,0 +1,26 @@
+local Data = require('stdlib/data/data')
+Data {
+    type = 'custom-input',
+    name = 'picker-itemswap-item-prev',
+    key_sequence = 'CONTROL + Y',
+    consuming = 'all'
+}
+
+Data {
+    type = 'custom-input',
+    name = 'picker-itemswap-item-next',
+    key_sequence = 'Y',
+    consuming = 'all'
+}
+Data {
+    type = 'custom-input',
+    name = 'picker-itemswap-group-prev',
+    key_sequence = 'CONTROL + SHIFT + Y',
+    consuming = 'all'
+}
+Data {
+    type = 'custom-input',
+    name = 'picker-itemswap-group-next',
+    key_sequence = 'SHIFT + Y',
+    consuming = 'all'
+}
