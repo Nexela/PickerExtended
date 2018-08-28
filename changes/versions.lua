9016c1cae42976b2483aa212d9cc695e1dba9db7
Nicholas Dunn <nexela@gmail.com>
2018-08-27 22:12:45 -0400

    Fix migration error

--- changes/versions.lua
+++ changes/versions.lua
@@ -9,10 +9,8 @@ return {
         global.notes_by_invis = global.notes_by_invis or {}
         global.notes_by_target = global.notes_by_target or {}
         global.n_note = global.n_note or 0
-    end
-
+    end,
     ['4.0.0'] = function()
-        for _, player in pairs(game.players) do
         global.notes_by_invis = nil
         global.notes_by_target = nil
         global.n_note = nil
