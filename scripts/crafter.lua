fd42b68ccb4eb874556ec8ff9edff94d38d93e87
Dimava <dimava2@ya.ru>
2018-12-31 13:11:23 +0300

    Implement picker-allow-multiple-craft

--- scripts/crafter.lua
+++ scripts/crafter.lua
@@ -7,8 +7,9 @@ local lib = require('scripts/lib')
 local function picker_crafter(event)
     local player = game.players[event.player_index]
     if player.selected and not player.cursor_stack.valid_for_read then
+        local multiple_enabled = player.mod_settings['picker-allow-multiple-craft'].value
         local _, _, ip = lib.get_placeable_item(player.selected)
-        if ip and game.recipe_prototypes[ip.name] and player.force.recipes[ip.name].enabled and player.get_item_count(ip.name) == 0 then
+        if ip and game.recipe_prototypes[ip.name] and player.force.recipes[ip.name].enabled and (multiple_enabled or player.get_item_count(ip.name) == 0) then
             player.begin_crafting {count = 1, recipe = ip.name, silent = false}
         end
     end
