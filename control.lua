916605d60ecc17273ea6b9b804959349f9682d89
Nicholas Dunn <nexela@gmail.com>
2019-05-25 18:38:59 -0400

    Fix Ooopsie

--- control.lua
+++ control.lua
@@ -2,7 +2,7 @@
 --[Picker Extended]--
 -------------------------------------------------------------------------------
 require('__stdlib__/stdlib/event/event').set_protected_mode(true)
-require('__stdlib__/stdlib/event/changes').register_events('fake', 'changes/versions')
+require('__stdlib__/stdlib/event/changes').register_events('mod_versions', 'changes/versions')
 require('__stdlib__/stdlib/event/player').register_events(true)
 require('__stdlib__/stdlib/event/force').register_events(true)
 
